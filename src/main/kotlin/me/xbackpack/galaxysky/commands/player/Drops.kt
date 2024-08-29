8fa8ffc56b61b15ba41b2c19449ef0ed36c39979
xBackpack <jamescheeseman21@gmail.com>
2024-08-29 15:03:17 +0100

    Added Drops command

---
+++ src/main/kotlin/me/xbackpack/galaxysky/commands/player/Drops.kt
@@ -0,0 +1,32 @@
+package me.xbackpack.galaxysky.commands.player
+
+import me.xbackpack.galaxysky.util.commandTypes.CooldownCommand
+import me.xbackpack.galaxysky.util.commandTypes.ListenerCommand
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerDropItemEvent
+
+class Drops :
+    CooldownCommand,
+    ListenerCommand {
+    override val commandName = "drops"
+    override val description = "Toggles the ability to drop items"
+    override val requiresPlayer = true
+    override val cooldownDuration = 200L
+    override val cooldownStartMessage = Component.text("You can now drop items!", NamedTextColor.GREEN)
+    override val cooldownMessage = Component.text("You can already drop items!", NamedTextColor.RED)
+    override val cooldownEndMessage = Component.text("You can no longer drop items!", NamedTextColor.RED)
+
+    init {
+        registerListener()
+    }
+
+    @EventHandler
+    fun onDrop(event: PlayerDropItemEvent) {
+        if (!isCooldown(event.player)) {
+            event.isCancelled = true
+            event.player.sendMessage(Component.text("Use /drops to drop items!", NamedTextColor.RED))
+        }
+    }
+}
