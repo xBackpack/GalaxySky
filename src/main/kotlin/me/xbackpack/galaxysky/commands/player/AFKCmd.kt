a59d2f9bc333a0e539b9cdfcd72d3e97adfb041b
xBackpack <jamescheeseman21@gmail.com>
2024-08-31 12:47:09 +0100

    Patched AFK Command

--- src/main/kotlin/me/xbackpack/galaxysky/commands/player/AFKCmd.kt
+++ src/main/kotlin/me/xbackpack/galaxysky/commands/player/AFKCmd.kt
@@ -3,11 +3,10 @@ package me.xbackpack.galaxysky.commands.player
 import me.xbackpack.galaxysky.util.Worlds
 import me.xbackpack.galaxysky.util.commandTypes.TeleportCommand
 import org.bukkit.Location
-import org.bukkit.entity.Player
 
 class AFKCmd : TeleportCommand {
     override val commandName = "afk"
     override val description = "Teleports the player to the AFK area"
 
-    override fun location(player: Player) = Location(Worlds.world, -28.5, 102.0, 10.5, 90f, 0f)
+    override val location = Location(Worlds.world, -28.5, 102.0, 10.5, 90f, 0f)
 }
