847783acea05f6aba5209ed5e675bbd9499194c0
xBackpack <jamescheeseman21@gmail.com>
2025-07-20 00:23:13 +0100

    Added Listener Registry

---
+++ src/main/kotlin/me/xbackpack/galaxysky/listener/ListenerRegistry.kt
@@ -0,0 +1,30 @@
+package me.xbackpack.galaxysky.listener
+
+import me.xbackpack.galaxysky.GalaxySky
+import me.xbackpack.galaxysky.common.Registry
+import org.bukkit.event.Event
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+
+interface ListenerRegistry : Registry {
+    companion object {
+        inline fun <reified T : Event> hookEvent(
+            priority: EventPriority = EventPriority.NORMAL,
+            ignoreCancelled: Boolean = false,
+            crossinline handler: (T) -> Unit,
+        ) {
+            val listener = object : Listener {}
+
+            GalaxySky.pluginManager.registerEvent(
+                T::class.java,
+                listener,
+                priority,
+                { _, event ->
+                    (event as? T)?.let { handler(it) }
+                },
+                GalaxySky.instance,
+                ignoreCancelled,
+            )
+        }
+    }
+}
