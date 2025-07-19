c07f0d3a12cd269e0e2cf8325b31ba93f714e00f
xBackpack <jamescheeseman21@gmail.com>
2025-07-20 00:22:14 +0100

    Added Listener for Joining and Leaving

--- src/main/kotlin/me/xbackpack/galaxysky/listener/PlayerListenerRegistry.kt
+++ src/main/kotlin/me/xbackpack/galaxysky/listener/PlayerListenerRegistry.kt
@@ -1,37 +1,104 @@
 package me.xbackpack.galaxysky.listener
 
-import me.xbackpack.galaxysky.GalaxySky
-import me.xbackpack.galaxysky.common.Registry
-import org.bukkit.event.Event
-import org.bukkit.event.EventPriority
-import org.bukkit.event.Listener
+import me.xbackpack.galaxysky.item.registry.Pickaxes
+import me.xbackpack.galaxysky.message.Message
+import me.xbackpack.galaxysky.service.LocationService
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.Bukkit
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 
-object PlayerListenerRegistry : Registry {
+object PlayerListenerRegistry : ListenerRegistry {
     override fun init() {
-        hookEvent<PlayerJoinEvent> { event ->
-            val plr = event.player
+        ListenerRegistry.hookEvent<PlayerJoinEvent> { event ->
+            val player = event.player
 
-            plr.sendMessage("You joined?")
+            event.joinMessage(
+                Message
+                    .create {
+                        section {
+                            text("[")
+
+                            text("+") {
+                                colour(NamedTextColor.GREEN)
+                            }
+
+                            text("]")
+
+                            colour(NamedTextColor.DARK_GRAY)
+                        }
+
+                        space()
+
+                        text(player.name) {
+                            colour(NamedTextColor.GRAY)
+                        }
+                    }.root,
+            )
+
+            if (!player.hasPlayedBefore()) {
+                event.joinMessage(
+                    Message
+                        .create {
+                            section {
+                                text("Welcome,")
+
+                                space()
+
+                                text(player.name) {
+                                    colour(NamedTextColor.LIGHT_PURPLE)
+                                }
+
+                                text(", to GalaxySky!")
+
+                                space()
+
+                                text("(")
+
+                                text("#${Bukkit.getOfflinePlayers().size}") {
+                                    colour(NamedTextColor.YELLOW)
+                                }
+
+                                text(")")
+
+                                colour(NamedTextColor.GRAY)
+                            }
+                        }.root,
+                )
+
+                val item = Pickaxes.STONE_PICKAXE_1
+
+                player.inventory.addItem(item.build())
+            }
+
+            player.teleport(LocationService.spawnLocation)
         }
-    }
 
-    private inline fun <reified T : Event> hookEvent(
-        priority: EventPriority = EventPriority.NORMAL,
-        ignoreCancelled: Boolean = false,
-        crossinline handler: (T) -> Unit,
-    ) {
-        val listener = object : Listener {}
+        ListenerRegistry.hookEvent<PlayerQuitEvent> { event ->
+            val player = event.player
 
-        GalaxySky.pluginManager.registerEvent(
-            T::class.java,
-            listener,
-            priority,
-            { _, event ->
-                (event as? T)?.let { handler(it) }
-            },
-            GalaxySky.instance,
-            ignoreCancelled,
-        )
+            event.quitMessage(
+                Message
+                    .create {
+                        section {
+                            text("[")
+
+                            text("-") {
+                                colour(NamedTextColor.RED)
+                            }
+
+                            text("]")
+
+                            colour(NamedTextColor.DARK_GRAY)
+                        }
+
+                        space()
+
+                        text(player.name) {
+                            colour(NamedTextColor.GRAY)
+                        }
+                    }.root,
+            )
+        }
     }
 }
