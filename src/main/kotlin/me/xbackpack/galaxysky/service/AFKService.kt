f8ab8c3345b2783323174f3d49fcb5ef46814d73
xBackpack <jamescheeseman21@gmail.com>
2025-07-21 00:43:14 +0100

    Updated AFK System

--- src/main/kotlin/me/xbackpack/galaxysky/service/AFKService.kt
+++ src/main/kotlin/me/xbackpack/galaxysky/service/AFKService.kt
@@ -1,11 +1,14 @@
 package me.xbackpack.galaxysky.service
 
 import me.xbackpack.galaxysky.hook.WorldGuardHook
+import me.xbackpack.galaxysky.item.api.giveItem
+import me.xbackpack.galaxysky.item.registry.Materials
 import me.xbackpack.galaxysky.message.Message
 import net.kyori.adventure.bossbar.BossBar
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.entity.Player
 import java.util.UUID
+import kotlin.math.ceil
 import kotlin.time.TimeMark
 import kotlin.time.TimeSource
 
@@ -14,60 +17,74 @@ object AFKService {
     private val bossbars = mutableMapOf<UUID, BossBar>()
 
     fun check(player: Player): Boolean {
+        val uuid = player.uniqueId
+
         val regions = WorldGuardHook.getRegionsFromLocation(player.location)
 
         val afkRegion = WorldGuardHook.getRegion(LocationService.world, "afk")
 
         if (!regions.contains(afkRegion)) {
-            hideBossbar(player)
+            bossbars[uuid]?.let {
+                player.hideBossBar(it)
+                bossbars.remove(uuid)
+            }
+
+            times.remove(uuid)
 
             return false
         }
 
-        times.putIfAbsent(player.uniqueId, TimeSource.Monotonic.markNow())
+        times.putIfAbsent(uuid, TimeSource.Monotonic.markNow())
 
-        showBossbar(player)
+        showBossbarAndHandlePayouts(player)
 
         return true
     }
 
-    fun showBossbar(player: Player) {
+    fun showBossbarAndHandlePayouts(player: Player) {
         val uuid = player.uniqueId
 
         val elapsed = times[uuid]?.elapsedNow() ?: error("Player $player is not in times dictionary")
 
-        val secondsPassed = elapsed.inWholeSeconds
+        val secondsPassed = ceil(elapsed.inWholeMilliseconds / 1000f) + 1
 
-        val progress = secondsPassed % 5f
+        val timeUntilNextPayout = ((secondsPassed - 1) % 120) + 1
+
+        val progress = timeUntilNextPayout / 120
 
         val title =
             Message
                 .create {
                     section {
-                        text("AFK Time")
+                        text("AFK Time:")
 
-                        text("$secondsPassed") {
-                            colour(NamedTextColor.AQUA)
+                        space()
+
+                        text(secondsPassed.toInt().toString()) {
+                            colour(NamedTextColor.GREEN)
                         }
 
                         text("s")
 
                         colour(NamedTextColor.LIGHT_PURPLE)
                     }
-                }
+                }.root
 
-        var bossbar = BossBar.bossBar(title.root, progress, BossBar.Color.PURPLE, BossBar.Overlay.PROGRESS)
+        bossbars[uuid]
+            ?.name(title)
+            ?.progress(progress)
+            ?: let {
+                val bossbar = BossBar.bossBar(title, progress, BossBar.Color.PURPLE, BossBar.Overlay.NOTCHED_12)
 
-        bossbars[uuid]?.let { bossbar = it.progress(progress) }
+                bossbars[uuid] = bossbar
 
-        bossbars[uuid] = bossbar
+                player.showBossBar(bossbar)
+            }
 
-        player.showBossBar(bossbar)
-    }
+        if ((secondsPassed != 1f) && (timeUntilNextPayout == 1f)) {
+            val item = Materials.AFK_TOKEN
 
-    fun hideBossbar(player: Player) {
-        val uuid = player.uniqueId
-
-        bossbars[uuid]?.let(player::hideBossBar)
+            player.giveItem(item)
+        }
     }
 }
